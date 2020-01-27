0aeade49ea0ec9352d59708c1e8e8a3411fe3458
arceda <enriquefirst@gmail.com>
2020-01-26 21:23:44 -0500

    definiendo la red neuronal del segundo articulo

--- Model-compression/detect-face.py
+++ Model-compression/detect-face.py
@@ -1,6 +1,15 @@
 import cv2
 from mtcnn import MTCNN
 
+def get_biggest_face(faces):
+    biggest_width = 0
+    biggest_face  = None
+    for face in faces:
+        x, y, width, height = face['box']
+        if width > biggest_width:
+            biggest_face = face
+
+    return biggest_face
 
 def draw_image_with_boxes(img, faces):	
     for face in faces:
@@ -12,7 +21,8 @@ def draw_image_with_boxes(img, faces):
 
 def get_eye_mouth(img, face):
     mouth_left_point = face['keypoints']['mouth_left']       
-    mouth_right_point = face['keypoints']['mouth_right']       
+    mouth_right_point = face['keypoints']['mouth_right']   
+    eye_left_point = face['keypoints']['left_eye']     
     
     mouth_width = mouth_right_point[0] - mouth_left_point[0]
     mouth_height = int(mouth_width*0.75)
@@ -24,11 +34,21 @@ def get_eye_mouth(img, face):
 
     mouth = img[ mouth_box_y:mouth_box_y+mouth_height, mouth_box_x:mouth_box_x+mouth_width]
 
-    print(mouth.shape)
-    cv2.imshow("mouth", mouth)
-    cv2.waitKey(30) 
+    ##############################################################################################
+    # estamos considerando el mismo tamaño de la boca para el ojo
+    eye_box_x = int(eye_left_point[0] - mouth_width/2)
+    eye_box_y = int(eye_left_point[1] - mouth_height/2)
+    
+    eye_width = mouth_width
+    eye_height = mouth_height
+
+    eye = img[ eye_box_y:eye_box_y+eye_height, eye_box_x:eye_box_x+eye_width]
+
+    return mouth, eye
+    
 
 
+'''
 img = cv2.cvtColor(cv2.imread("img.jpg"), cv2.COLOR_BGR2RGB)
 detector = MTCNN()
 faces = detector.detect_faces(img)
@@ -40,8 +60,8 @@ draw_image_with_boxes(img, faces)
 cv2.imshow("Frame", img)
 cv2.waitKey(0) 
 cv2.destroyAllWindows() 
-
-
+'''
+detector = MTCNN()
 cap = cv2.VideoCapture('../Eye-blink-detection/blink_detection_demo.mp4')
 
 while True:
@@ -51,12 +71,16 @@ while True:
         faces = detector.detect_faces(img)
 
         draw_image_with_boxes(img, faces)
-        for face in faces:
-            get_eye_mouth(img, face)
+
+        face = get_biggest_face(faces)
+        if (face != None):
+            mouth, eye = get_eye_mouth(img, face)
+
+            cv2.imshow("mouth", mouth)
+            cv2.imshow("eye", eye)
 
         cv2.imshow("Frame", img)
         key = cv2.waitKey(30) 
-
         if key == ord("q"):
             break
     else:
